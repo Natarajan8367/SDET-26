58898374f2978b6d6017524d33b4c7c3fb385f65
Admin <Admin@DESKTOP-IRLAT8F>
2021-12-13 15:01:06 +0530

    batch testing

---
+++ src/test/java/maven/Jenkins.java
@@ -0,0 +1,11 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Jenkins {
+	@Test
+	public void jenkins() {
+		 System.out.println("Welcome to Jenkins");
+		 System.out.println("Good Afternoon");
+   }
+}
