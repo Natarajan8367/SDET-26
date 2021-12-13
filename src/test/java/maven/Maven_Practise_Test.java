da32c25fa7bb90c7780406414053b375f4f9506e
Admin <Admin@DESKTOP-IRLAT8F>
2021-12-13 12:59:59 +0530

    New Push

---
+++ src/test/java/maven/Maven_Practise_Test.java
@@ -0,0 +1,14 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Maven_Practise_Test {
+@Test
+public void MavenPractise() {
+	 System.out.println("PUSH & COMMIT");
+	 System.out.println("PULL");
+	 System.out.println("Welcome to Test Yantra");
+	 System.out.println("Hellllllllllllllllo");
+	 System.out.println("Good Morning");
+	}
+}
