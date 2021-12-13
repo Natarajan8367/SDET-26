41936ea72c10fd29ff8b705a417fb9a90dda6c84
Admin <Admin@DESKTOP-IRLAT8F>
2021-12-13 13:20:56 +0530

    Push_Newm

---
+++ src/test/java/maven/Practise_Test_2.java
@@ -0,0 +1,11 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class Practise_Test_2 {
+	@Test
+	public void MavenPractise() {
+		 System.out.println("Welcome to Test Yantra");
+		 System.out.println("Good Morning");
+		}
+}
